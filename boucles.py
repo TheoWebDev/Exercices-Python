e47d285b2973ec53ed6375d0ae655f76421f24b1
Théo <git@init.com>
2021-07-20 13:48:11 +0200

    Mise à jour

--- boucles.py
+++ boucles.py
@@ -2,14 +2,14 @@
 
 # EXERCICE : Boucle FOR table de multiplication
 
-# def table_multiplication(number, maxCalcul = 11):
-#     for i in range(1, maxCalcul):
-#         print(i, " x ", number, " = ", i * number)
+def table_multiplication(number, maxCalcul = 11):
+    for i in range(1, maxCalcul):
+        print(i, " x ", number, " = ", i * number)
 
 # Fonction range pour le calcul d'un nombre entier
 # Ne pas utiliser print pour appeler la fonction table_multiplication
 
-# table_multiplication(9)
+table_multiplication(9)
 
 # EXERCICE : Demander un mot de passe pour rentrer dans la boucle
 
