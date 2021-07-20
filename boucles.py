3f00793bc0d39ab99e619fb01d62773341bb0944
Théo <git@init.com>
2021-07-20 13:47:09 +0200

    Mise à jour

--- boucles.py
+++ boucles.py
@@ -2,11 +2,26 @@
 
 # EXERCICE : Boucle FOR table de multiplication
 
-def table_multiplication(number, maxCalcul = 11):
-    for i in range(1, maxCalcul):
-        print(i, " x ", number, " = ", i * number)
+# def table_multiplication(number, maxCalcul = 11):
+#     for i in range(1, maxCalcul):
+#         print(i, " x ", number, " = ", i * number)
 
 # Fonction range pour le calcul d'un nombre entier
 # Ne pas utiliser print pour appeler la fonction table_multiplication
 
-table_multiplication(8)
+# table_multiplication(9)
+
+# EXERCICE : Demander un mot de passe pour rentrer dans la boucle
+
+def table_with_password():
+    password = input("Enter your password ? ")
+    while password != "python":
+        password = input("Enter your password ? ")
+    i = 1
+    while i < 6:
+        print(i)
+        i += 1
+
+table_with_password()
+
+# Tant que le bon mdp n'est pas donné, 1ère boucle while, si bon mdp, affiche 2ème boucle while
